ed8d76c4abd15ae54b1fd9d75353aef125d1752e
Simon Parsons <simon.parsons@hippodigital.co.uk>
2026-08-10 08:48:21 +0100

    update

--- generate_achievement_icons/go.ps1
+++ generate_achievement_icons/go.ps1
@@ -1,4 +1,4 @@
-$env:OPENAI_API_KEY = "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"
+$env:OPENAI_API_KEY = "<insert open api key here>"
 
 npx tsx .\achievement-medals.ts .\achievements.json .\achievement-medals
 
